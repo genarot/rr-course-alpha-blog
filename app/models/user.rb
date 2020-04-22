d18633de4f47f320713755cd01daff0ac2f58aa0
gtinoco <genaro_13@hotmail.es>
2020-04-21 23:35:37 -0600

    Added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
+  validates :username, presence: true,
+            length: {minimum: 3, maximum: 25},
+            uniqueness: {case_sensitive: false}
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]{2,} \z/i
+  validates :email, presence: true,
+            length: {minimum: 10, maximum: 150},
+            uniqueness: {case_sensitive: false},
+            format: {with: VALID_EMAIL_REGEX, }
 
 end
