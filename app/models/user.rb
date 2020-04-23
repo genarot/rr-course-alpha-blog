43238601445456ac906b893261a14c53884b3495
gtinoco <genaro_13@hotmail.es>
2020-04-22 22:18:44 -0600

    Added secure password to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,16 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase}
   has_many :articles
 
   validates :username, presence: true,
             length: {minimum: 3, maximum: 25},
             uniqueness: {case_sensitive: false}
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]{2,} \z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-zA-Z\d\-]+(\.[a-z\d\-]+)*\.[a-z]{2,}\z/i
   validates :email, presence: true,
             length: {minimum: 10, maximum: 150},
             uniqueness: {case_sensitive: false},
             format: {with: VALID_EMAIL_REGEX, }
 
+   has_secure_password
 end
